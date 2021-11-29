17b339710c66d8d6f3897fd7593a6f485c3423d3
Ivan Šarić <ivan@path-variable.com>
2021-11-29 13:12:21 +0100

    add screenshot

--- motion-poll.go
+++ motion-poll.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 	"time"
@@ -22,8 +23,9 @@ import (
      2 - password
      3 - camera name (or location)
      4 - slack hook url
-     5 - (optional) cooldown time after motion event detected
-     6 - (optional) json message template for sprintf ex. ./motion-poll "${<file.json}"
+	 5 - snapshot save path
+     6 - (optional) cooldown time after motion event detected
+     7 - (optional) json message template for sprintf ex. ./motion-poll "${<file.json}"
  */
 func main() {
 	// get and validate number of cli args
@@ -49,8 +51,8 @@ func main() {
 	// get slack message from template - use default if cli arg is not given
 	camName := args[3]
 	var msgT string
-	if len(args) > 6 {
-		msgT = args[6]
+	if len(args) > 7 {
+		msgT = args[7]
 	} else {
 		msgT = ` 
     	{
@@ -61,8 +63,8 @@ func main() {
 
 	//get cooldown time from args, default 10 seconds
 	cooldown := 10
-	if len(args) > 5 {
-		convInt, err := strconv.Atoi(args[5])
+	if len(args) > 6 {
+		convInt, err := strconv.Atoi(args[6])
 		if err == nil {
 			cooldown = convInt
 		} else {
@@ -77,15 +79,27 @@ func main() {
 		bodyBytes, _ := ioutil.ReadAll(r2.Body)
 		bodyS := string(bodyBytes)
 		if strings.Contains(bodyS, "<tt:SimpleItem Name=\"IsMotion\" Value=\"true\" />") {
-			fmt.Printf("%s", bodyS)
 			msg := fmt.Sprintf(msgT, camName)
 			_, err = http.Post(args[4], "aplication/json", bytes.NewReader([]byte(msg)))
 			if err != nil {
 				fmt.Printf("there was an error while posting the slack notification %s", err)
 			}
+			captureScreenshot(args[0], args[1], args[2], args[5])
 			time.Sleep(time.Duration(cooldown) * time.Second)
 		}
 		time.Sleep(1 * time.Second)
 	}
 
+}
+
+func captureScreenshot(url, user, pass, imgPath string) {
+	path := fmt.Sprintf("%s/%s.jpeg",imgPath, time.Now().Format("20060102150405"))
+	curl := strings.Split(url, ":")[0]
+	rurl := fmt.Sprintf("rtsp://%s/user=%s_password=%s_channel=1_stream=1.sdp", curl, user, pass)
+	fmt.Printf("taking screenshot %s\n", rurl)
+	cmd := exec.Command("ffmpeg", "-y", "-i", rurl, "-vframes","1", path)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Screnshot error is %s", err)
+	}
 }
